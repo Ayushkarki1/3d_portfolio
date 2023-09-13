5297b0b972e482eb12d99e5752fd98117dc36728
Ayushkarki1 <supremesonam2019@gmail.com>
2023-09-12 22:46:18 -0400

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,87 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import c from "./tech/c.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import py from "./tech/py.png";
+import ups from "./company/ups.png"; 
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import chatapp from "./chatapp.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import passwordgen from "./passwordgen.png"
+import Weatherapp from "./Weatherapp.png"
+import poke from "./poke.jpg"
+import tictactoee from "./tictactoee.png"
+import tshirt from "./tshirt.png"
+import LoginandRegistrationwebapp from './LoginandRegistrationwebapp.png'
+import hang from './hang.jpg'
+import unnamed from './unnamed.png'
+import threedp from "./threedp.png";
+
+
+export {
+  logo,
+  threedp,
+  ups,
+  unnamed,
+  tshirt,
+  tictactoee,
+  c,
+  poke,
+  hang,
+  py,
+  passwordgen,
+  Weatherapp,
+  backend,
+  creator,
+  mobile,
+  web,
+  LoginandRegistrationwebapp,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  jobit,
+  chatapp,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,349 @@
+import {
+    mobile,
+    poke,
+    ups,
+    backend,
+    creator,
+    unnamed,
+    hang,
+    tshirt,
+    Weatherapp, 
+    passwordgen,
+    web,
+    LoginandRegistrationwebapp,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    threedp,
+    chatapp,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    tictactoee,
+    c,
+    py,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Full stack Developer",
+      icon: backend,
+    },
+    
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "Python",
+      icon: py,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "c++",
+      icon: c,
+    },
+    
+   
+  ];
+  
+  const experiences = [
+    {
+      title: "Volunteer",
+      company_name: "American Lung Association",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "june 2019 ",
+      points: [
+        "helped out in the office for some time as an assistant.",
+      ],
+    },   
+    {
+      title: "Package Handler",
+      company_name: "UPS",
+      icon: ups,
+      iconBg: "#E6DEDD",
+      date: "Sept 2020 - Oct 2020",
+      points: [
+        "As a package handler in a warehouse, my primary responsibility was to efficiently and safely handle incoming and outgoing shipments. This role required a strong attention to detail and physical stamina. I would often unload packages from trucks, sort them based on size and destination, and then load them onto the appropriate delivery vehicles.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {}
+  ];
+  
+  const projects = [
+    {
+      name: "3-D portfolio ",
+      description:
+        "My very own 3-D portfolio that displays all my  which some are shown in 3-D animated, demonstrating a solid foundation of basic 3d modeling skills ",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "node",
+          color: "green-text-gradient",
+        },
+        {
+          name: "chatengine.io",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: threedp,
+      source_code_link: "https://github.com/Ayushkarki1/chatapp/tree/main",
+    },
+    {
+      name: "full stack Chatting web app ",
+      description:
+        "full stack Chatting web app using Node JS, React JS and ChatEngine.io- an app where u can invite a friend and have a chat, upload files and create group chats.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "node",
+          color: "green-text-gradient",
+        },
+        {
+          name: "chatengine.io",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: chatapp,
+      source_code_link: "https://github.com/Ayushkarki1/chatapp/tree/main",
+    },
+    {
+      name: "3-D T-shirt Customizer",
+      description:
+        "A 3-d model t shirst which allows you to view the t shirt in 360 angle. Also user can set the color and upload logo/designs they wish",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tshirt,
+      source_code_link: "https://github.com/Ayushkarki1/threejs",
+    },
+    {
+      name: "Login and Registration tool",
+      description:
+        "Login and Registration using MERN Stack | Mongo, Express, React and Node Authentication",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: LoginandRegistrationwebapp,
+      source_code_link: "https://github.com/Ayushkarki1/threejs",
+    },
+    {
+      name: "Profemon",
+      description:
+        "",
+      tags: [
+        {
+          name: "c++",
+          color: "blue-text-gradient",
+        },
+        
+      ],
+      image: poke,
+      source_code_link: "https://github.com/Ayushkarki1/profemoncpp",
+    },
+    {
+      name: "Hangman",
+      description:
+        ".",
+      tags: [
+        {
+          name: "c++",
+          color: "blue-text-gradient",
+        },
+        
+      ],
+      image: hang,
+      source_code_link: "https://github.com/Ayushkarki1/hangmangamec--",
+    },
+    {
+      name: "Password/Passcode generator",
+      description:
+        "user can generate unique password/passcode depending on their preference",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: passwordgen,
+      source_code_link: "https://github.com/Ayushkarki1/passwordgen",
+    },
+    {
+      name: "Weather web app",
+      description:
+        "displays weather of any location typed in by the user with the help of weatherapi",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: Weatherapp,
+      source_code_link: "https://github.com/Ayushkarki1/Weather-App",
+    },
+    {
+      name: "Unbeatable TicTacToe",
+      description:
+        "play a game of TicTacToe against an ai but odds are you wont win",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tictactoee,
+      source_code_link: "https://github.com/Ayushkarki1/Weather-App",
+    },
+    {
+      name: "2-D car game",
+      description:
+        "",
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+        
+      ],
+      image: unnamed,
+      source_code_link: "https://github.com/Ayushkarki1/raceproject",
+    },
+    
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
